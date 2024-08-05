dad2339db69756cf0252a4dd3bc1a276968ce37b
McBubski <treysen.wardle@gmail.com>
2024-08-05 13:55:08 -0600

    test!

---
+++ src/main.cpp
@@ -0,0 +1,32 @@
+#include "vex.h"
+
+using namespace vex;
+
+competition Competition;
+
+void pre_auton(void) {
+
+  vexcodeInit();
+}
+
+void autonomous(void) {
+
+}
+
+void usercontrol(void) {
+  while (1) {
+    wait(20, msec);
+
+  }
+}
+
+int main() {
+  Competition.autonomous(autonomous);
+  Competition.drivercontrol(usercontrol);
+
+  pre_auton();
+
+  while (true) {
+    wait(100, msec);
+  }
+}
